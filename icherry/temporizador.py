5269f14e7fd0856e36830beda2812cc47147217a
xolodho <xolodho@gmail.com>
2014-09-22 06:24:19 -0300

    Temporizador : actualizado con duraciones y velocidad (escala de tiempo)
    La versión original del temporizador se programaba con una cantidad en
    segundos (un número), pero ahora que contamos con duraciones en
    distintas unidades, podemos aprovecharlas para el temporizador. Ahora,
    el método 'ejecutarCada' toma una instancia de 'Duracion' en lugar de un
    número (representando segundos). Esto aumenta la claridad y es más
    cómodo.
    
    Por otro lado, la duración puede interpretarse en una escala dada, lo
    cual permite reinterpretar el significado de un 'segundo' de la duración
    en términos del tiempo del sistema operativo. Esto técnicamente no es
    necesario ya que siempre el llamador puede por su cuenta efectuar el
    escalamiento, pero creo que no está mal tenerlo acá, sobre todo si los
    temporizadores se usaran en un entorno de simulación en que el tiempo
    puede comprimirse o dilatarse (con respecto al tiempo real), los
    temporizadores se inicializan todos con un mismo factor de escala y se
    encargan ellos del ajuste.
    
    Ahora también se pueden pasar argumentos en la programación de la
    tarea (sólo necesario si se programa una tarea que requiere argumentos).
    
    Las tareas ahora pueden detener al temporizador que los programó.
    
    La demo también fue aumentada para mostrar los cambios.
    
    Detalle trivial: los nombres de los métodos de interfaz ahora comienzan
    con minúscula.

--- icherry/temporizador.py
+++ icherry/temporizador.py
@@ -7,30 +7,46 @@
 
 # La versión actual del temporizador sólo puede programar una única
 # función (via threading.timer), había una versión previa que podía
-# schedulear varias funciones al mismo tiempo (via sched.scheduler),
-# garantizando una ejecución serial de las mismas. Si se quieren
-# schedulear N tareas, en esta versión deben usarse N temporizadores
-# (cada uno requerirá un thread, y no se garantiza una ejecución
-# serial, es decir puede haber race conditions si las tareas acceden a
-# una región crítica).
+# schedulear varias funciones (via sched.scheduler) garantizando una
+# ejecución serial de éstas. Si se quieren schedulear N tareas, en
+# esta versión deben usarse N temporizadores (cada uno creará un
+# thread, y no se garantiza una ejecución serial entre las tareas de
+# los distintos temporizadores (son independientes), es decir puede
+# haber race conditions si las tareas acceden a una región crítica).
 #
 # Actualmente, el protocolo de temporización consiste en tres pasos:
-# primero instanciar el temporizador (sin argumentos), luego programar
-# la temporización y finalmente activarlo (ver ejemplo a
-# continuación):
+# primero instanciar el temporizador (sin argumentos o con un
+# argumento de velocidad para acelerar o desacelerar la
+# temporización), luego programar la temporización (cuánto tiempo, qué
+# función y opcionalmente qué argumentos) y finalmente activarlo (ver
+# ejemplo a continuación):
 
 # t = Temporizador()
-# t.EjecutarCada(tiempo, funcion)
-# t.IniciarEjecucion()
+# t.ejecutarCada(duracion, funcion)
+# t.iniciarEjecucion()
 
-# El tiempo es en segundos. La función puede ser un lambda o una
-# función definida con 'def' (esto asume un diseño en que no hay un
-# concepto de 'closure').
+# El argumento 'duracion' es cualquier instancia de Duracion, es
+# independiente de la unidad. La función puede ser un lambda o una
+# función definida con 'def'. Si hay argumentos para la función, se
+# pasan en una lista como último argumento.
 
-# En cualquier momento puede llamarse a 'EjecutarCada' para crear un
+# En cualquier momento puede llamarse a 'ejecutarCada' para crear un
 # nuevo scheduling (si había una tarea programada, se cancela
-# automáticamente). Además, el método 'Detener' sirve para detener el
-# temporizador (cancela la programación de la tarea, si había).
+# automáticamente). Además, el método 'detener' sirve para detener el
+# temporizador (cancela la programación de la tarea, si había), en
+# este estado puede volver a programarse una tarea (aunque no es
+# necesario detenerla para hacer esto, por lo recién explicado).
+
+# Notar que el hecho de que un temporizador tenga la facultad de
+# ejecutar una tarea que lo detenga permite ejecutar tareas una
+# cantidad finita (definida) de veces, por ejemplo, para ejecutar una
+# tarea una única vez, la función parámetro puede consistir en
+# ejecutar la tarea propiamente dicha seguida de una detención del
+# temporizador. No es lo más elegante pero funciona si se hace con
+# cuidado.
+
+# En esta versión no puede modificarse la velocidad, sólo puede
+# pasarse como parámetro durante la construcción.
 
 # Ver demo al final.
 
@@ -40,39 +56,58 @@ import threading
 
 class Temporizador :
     """Un temporizador: se utiliza para ejecutar una tarea en forma
-    repetida con un período dado. Primero debe definirse la tarea (una
-    función), finalmente se pone a ejecutar el temporizador. La tarea
-    se ejecutará en su propio thread.
+    repetida con un período dado (una duración) y opcionalmente a una
+    velocidad dada (cuántos segundos de duración por segundo de la
+    realidad, usado para reescalar el tiempo). Primero debe definirse
+    la tarea (una función), finalmente se pone a ejecutar el
+    temporizador. La tarea se ejecutará en su propio thread.
 
     """
 
-    def __init__(self) :
-        # usamos el scheduler provisto por Python
-        # self.scheduler = sched.scheduler()
+    def __init__(self, velocidad = 1.0) :
+        # usamos el timer provisto por Python
         self.timer = None
+        assert(velocidad > 0)
+        self._velocidad = velocidad
 
-    def _action(self,tiempo,funcion) :
+    def _action(self,segundos,funcion,args) :
         # esta función es un pequeño hack para reschedulear la
         # función, no forma parte de la interfaz.
-        funcion()               # llamamos a la función primero
-        self.EjecutarCada(tiempo,funcion)  # y la rescheduleamos
-        self.timer.start()
+        funcion(*args)               # llamamos a la función primero
+        # podría ser que la función recién ejecutada haya tenido como
+        # efecto lateral detener a este temporizador. Rescheduleamos
+        # sólo si no está detenido. Así permitimos que un temporizador
+        # planifique su propia detención.
+        if self.timer is not None:
+            self._ejecutarCada(segundos,funcion,args)  # la rescheduleamos
+            self.timer.start()                         # y activamos
 
-    def EjecutarCada(self,tiempo,funcion) :
+    def _ejecutarCada(self,segundos,funcion,args) :
+        self.timer = threading.Timer(segundos,self._action,[segundos,funcion,args])
+
+    def ejecutarCada(self,duracion,funcion,args=None) :
         """Encarga una función para ser ejecutada con un período dado
 
         """
         # primero cancelamos el timer si estaba ya instanciado
         if self.timer : self.timer.cancel()
-        self.timer = threading.Timer(tiempo,self._action,[tiempo,funcion])
+        # convertimos a segundos y le damos rosca para que empiece el
+        # ciclo de temporizaciones.
+        segundos = duracion.aSegundos().valor() / self._velocidad
+        if args is None : args = []
+        self._ejecutarCada(segundos,funcion,args)
 
-    def Detener(self) :
+    def detener(self) :
         """Detiene el temporizador, si había una tarea pendiente se cancela.
+        Siempre puede volver a programarse una tarea con un
+        temporizador detenido (como si estuviese recién construido),
+        pero no es necesario detenerlo para hacer una reprogramación.
 
         """
         self.timer.cancel()
+        self.timer = None       # con esto indicamos que está detenido
 
-    def IniciarEjecucion(self) :
+    def iniciarEjecucion(self) :
         """Pone en marcha el temporizador.
 
         """
@@ -81,30 +116,61 @@ class Temporizador :
 # ====================================================================
 # mini demo (para correr en la repl)
 
-# Lo que hace es ejecutar un par de tareas a intervalos de 2 y 4
-# segundos respectivamente (se imprimen los minutos y segundos para
+# Lo que hace es ejecutar algunas tareas a distintos intervalos
+# de unos pocos segundos (se imprimen los minutos y segundos para
 # mayor claridad). Luego de unos instantes se detienen los
 # temporizadores (esto está programado también). Al ejecutarlo en la
-# repl, hay un total de 4 threads hasta que sólo queda activo el
+# repl, se crean varios threads hasta que sólo queda activo el
 # thread de la repl.
-
-def detener(t1,t2) :
-    t1.Detener()
-    t2.Detener()
-    print("terminado!")
 
 # una pruebita para ver cómo anda
 def demo() :
     from time import gmtime, strftime
-    t1 = Temporizador()
-    t2 = Temporizador()
-    t1.EjecutarCada(2,lambda:print("2: ",strftime("%M:%S", gmtime())))
-    t2.EjecutarCada(4,lambda:print("4: ",strftime("%M:%S", gmtime())))
-    t1.IniciarEjecucion()
-    t2.IniciarEjecucion()
-    s = threading.Timer(16,detener,args=[t1,t2])
-    s.start()
-    print("hay %d threads en ejecución" % threading.active_count())
+    from tiempo import DuracionEnSegundos
+
+    #
+    # 0. inicialización de algunas mierditas
+    #
+    class Contador : pass
+    def incrementar(contador) :
+        contador.i += 1
+    contador = Contador()
+    contador.i = 0
+    def detener(temporizadores,contador) :
+        # detiene los temporizadores e imprime el valor del contador.
+        for t in temporizadores :
+            t.detener()
+        print("terminado. contador = %d" % contador.i)
+
+    #
+    # 1. creamos los temporizadores
+    #
+    velocidad = 2             # 1.0 = escala real, 2.0 = doblemente rápido
+    t1 = Temporizador(velocidad)
+    t2 = Temporizador(velocidad)
+    t3 = Temporizador(velocidad)
+    t4 = Temporizador(velocidad)
+    temporizadores = [t1,t2,t3,t4]
+
+    #
+    # 2. programamos los temporizadores
+    #
+    t1.ejecutarCada(DuracionEnSegundos(2),
+                    lambda:print( "2      : ",strftime("%M:%S", gmtime())))
+    t2.ejecutarCada(DuracionEnSegundos(4),
+                    lambda:print( "4      : ",strftime("%M:%S", gmtime())))
+    t3.ejecutarCada(DuracionEnSegundos(1), incrementar, [contador])
+    t4.ejecutarCada(DuracionEnSegundos(12), detener, [temporizadores,contador])
+    # notar que t4 detiene a todos los temporizadores, incluso se
+    # detiene a sí mismo.
+
+    #
+    # 3. ponemos los temporizadores a ejecutar
+    #
+    print("ejecutando temporizadores a velocidad = %.2f" % velocidad)
+    for t in temporizadores : t.iniciarEjecucion()
+    print("hay %d temporizadores y %d threads en ejecución" %
+          (len(temporizadores), threading.active_count()))
 
 if __name__ == "__main__" : demo()
 
